ca55ffe63abde023bbefc07ab17c3ebe053d9014
kirilllllllllllllllllllllllllllll <150334314+kirilllllllllllllllllllllllllllll@users.noreply.github.com>
2024-03-02 20:18:21 +0700

    Add files via upload

--- data/exersize.py
+++ data/exersize.py
@@ -1,17 +1,25 @@
 import sqlalchemy
 from .db_session import SqlAlchemyBase
 
-class Exersize(SqlAlchemyBase):
+class Exercise(SqlAlchemyBase):
     __tablename__ = 'exersize'
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
-    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    name = sqlalchemy.Column(sqlalchemy.String)
 
-    teacher = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    teacher = sqlalchemy.Column(sqlalchemy.String)
     
     students = sqlalchemy.Column(sqlalchemy.String, nullable=True)
 
-    content = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    content = sqlalchemy.Column(sqlalchemy.String)
+
+    right_answer = sqlalchemy.Column(sqlalchemy.String)
+
+    results = sqlalchemy.Column(sqlalchemy.String)
+
+    name_teacher = sqlalchemy.Column(sqlalchemy.String)
+
+    surname_teacher = sqlalchemy.Column(sqlalchemy.String)
 
     file = sqlalchemy.Column(sqlalchemy.String, nullable=True)
